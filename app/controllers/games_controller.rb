9980061f70a3179fc40e592e678e0e20295bebc9
Louis Lescure <louis.lescure@mail.mcgill.ca>
2020-01-15 15:25:24 +0100

    added create game action and view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,26 @@
 class GamesController < ApplicationController
+  def create
+    @new_game = Game.new(game_params)
+    @new_game.start = Date.today
+    @new_game.save
+    redirect_to game_path(@new_game)
+  end
+
+  def new
+    @game = Game.new
+    4.times { @game.players.build }
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:date, players_attributes: [:name])
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,7 @@
 class Game < ApplicationRecord
   has_many :players, dependent: :destroy
   has_many :rounds, dependent: :destroy
+
+  accepts_nested_attributes_for :players
+
 end
